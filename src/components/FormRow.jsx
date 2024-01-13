46069be59588cfd65b9b2ad28c756ea7b1e87e04
ryanjin333 <ryanjin333@gmail.com>
2024-01-13 12:35:06 -0500

    Login screen added

---
+++ src/components/FormRow.jsx
@@ -0,0 +1,34 @@
+import { View, Text, TextInput } from "react-native";
+import React from "react";
+import { BlurView } from "expo-blur";
+
+import { Inter_400Regular, useFonts } from "@expo-google-fonts/inter";
+
+const FormRow = ({ name, placeholder, value, isPassword, handleChange }) => {
+  // fonts
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <View className="w-full rounded-2xl overflow-hidden">
+      <BlurView intensity={30}>
+        <TextInput
+          className="h-16 text-sm bg-transparent text-white px-4"
+          placeholderTextColor="#7C7C7C"
+          placeholder={placeholder}
+          secureTextEntry={isPassword ? true : false}
+          onChangeText={(value) =>
+            handleChange(value, name.charAt(0).toLowerCase() + name.slice(1))
+          }
+          value={value}
+        ></TextInput>
+      </BlurView>
+    </View>
+  );
+};
+
+export default FormRow;
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FormRow } from "../components";
 
 export default function Login() {
+  const handleChange = () => {
+    print("hi");
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-black px-8">
+      <Text className="text-white text-5xl">Login</Text>
+      <FormRow
+        name="Email"
+        placeholder="Enter your email"
+        handleChange={handleChange}
+        value="email"
+      />
+    </SafeAreaView>
   );
 }
