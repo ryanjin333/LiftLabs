bb87e5ec727664c822d909c6b22722ebcda23b30
ryanjin333 <ryanjin333@gmail.com>
2024-05-25 22:29:20 -0400

    Begin Working on Focus Screen/Mode #29

--- src/screens/Focus.jsx
+++ src/screens/Focus.jsx
@@ -1,26 +1,66 @@
-import { View, Text, Dimensions, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  FlatList,
+  Pressable,
+  Image,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 
-const data = [
-  { key: "1", text: "Page 1" },
-  { key: "2", text: "Page 2" },
-  { key: "3", text: "Page 3" },
-  // Add more data as needed
-];
+// redux imports
+import { useDispatch, useSelector } from "react-redux";
 
-const Focus = () => {
+const Focus = ({ navigation }) => {
+  // redux
+  const dispatch = useDispatch();
+  const currentWorkout = useSelector((state) => state.exercise.currentWorkout);
   return (
     <FlatList
       className="bg-black"
-      data={data}
-      keyExtractor={(item) => item.key}
+      data={currentWorkout.plan}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <View className="h-screen w-screen justify-center items-center bg-black">
-          <SafeAreaView>
-            <Text className="text-white">{item.text}</Text>
-          </SafeAreaView>
-        </View>
+        // actual screen
+        <SafeAreaView className="h-screen w-screen p-6 bg-black">
+          {/* main title and exit button */}
+          <View className="flex-row justify-between">
+            {/* title  */}
+            <Text className="w-72 text-white text-primary font-interBold text-3xl">
+              {item.sets}
+              <Text className="text-white font-interMedium text-2xl">
+                {" "}
+                sets of{"\n"}
+              </Text>
+              {item.title}
+            </Text>
+            {/* exit button */}
+            <Pressable
+              className="w-11 h-11 flex justify-start items-end "
+              onPress={() => navigation.goBack()}
+            >
+              <Image
+                className="h-6 w-6"
+                source={require("../assets/exit.png")}
+              />
+            </Pressable>
+          </View>
+
+          {/* other information */}
+
+          {/* exercise reps */}
+          <Text className="text-white text-primary font-interBold text-3xl mt-10">
+            {item.reps}
+            <Text className="text-white font-interMedium text-2xl"> reps</Text>
+          </Text>
+
+          {/* exercise weight */}
+          <Text className="text-white text-primary font-interBold text-3xl ">
+            {item.weight}
+            <Text className="text-white font-interMedium text-2xl"> lbs</Text>
+          </Text>
+        </SafeAreaView>
       )}
       snapToInterval={Dimensions.get("window").height}
       decelerationRate="fast"
